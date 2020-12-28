ec5dd7cad8b22d8456e6de1566fe3b6a5b1befac
Jared Norman <jared@super.gd>
2020-12-28 22:37:52 +0000

    Initial commit

---
+++ lib/good_luck_wizard.rb
@@ -0,0 +1,6 @@
+require "good_luck_wizard/version"
+
+module GoodLuckWizard
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,4 @@
+$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+require "good_luck_wizard"
+
+require "minitest/autorun"
